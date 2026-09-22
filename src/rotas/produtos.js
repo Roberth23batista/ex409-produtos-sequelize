ff614bb75685706b022baa50d32dc02aa80f0ce8
Roberth <2025300459@MACEIO.IFAL>
2026-09-22 14:49:18 -0300

    sim

--- src/rotas/produtos.js
+++ src/rotas/produtos.js
@@ -20,32 +20,100 @@ const router = express.Router();
 //  DELETE /produtos/:id     → 204 (sem corpo); 404 se não existir
 
 // GET /produtos — lista todos
-router.get("/", async (req, res) => {
+router.get("/produto", async (req, res) => {
+  const produto = await Produto.findAll();
+  res.status(200).json(produto);
   // TODO: retorne 200 com todos os produtos (Produto.findAll()).
 });
 
 // GET /produtos/:id — um produto
-router.get("/:id", async (req, res) => {
+router.get("/produto/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const produto = await Produto.findByPk(id)
+  if(produto){
+    res.status(200).json(produto)
+  }else{
+    res.status(404).json("Produto não encontrado")
+  }
   // TODO: busque por id; 200 com o produto ou 404 se não existir.
 });
 
 // POST /produtos — cria
-router.post("/", async (req, res) => {
+router.post("/produto", async (req, res) => {
+  const { descricao } = req.body;
+  const { preco } = req.body;
+  
+  if(descricao == null || preco == null){
+    res.status(400).json("Descrição e/ou preço faltando")
+
+  }else{
+    const produto = await Produto.create({ descricao }, { preco });
+    res.status(201).json(produto)
+
+  }
   // TODO: valide descricao e preco (400 se faltar); crie e responda 201 com o produto.
 });
 
 // PUT /produtos/:id — substitui (descricao e preco obrigatórios)
-router.put("/:id", async (req, res) => {
+router.put("/produto/:id", async (req, res) => {
+
+  const { descricao } = req.body;
+  const { preco } = req.body;
+  const { id } = req.params;
+  
+  if(descricao == null || preco == null){
+    res.status(400).json("Descrição e/ou preço faltando")
+
+  }
+
+  const produto = await Produto.findByPk(id);
+  if(produto == null){
+    res.status(400).json("Produto não encontrado")
+  }else{
+    produto.descricao = descricao;
+    produto.preco = preco;
+
+    await produto.save();
+
+    res.status(200).json(produto);
+  }
   // TODO: 404 se não existir; 400 se faltar campo; senão atualize e responda 200.
 });
 
 // PATCH /produtos/:id — atualização parcial
-router.patch("/:id", async (req, res) => {
+router.patch("/produto/:id", async (req, res) => {
+  const { id } = req.params;
+  const { descricao } = req.body;
+  const { preco } = req.body;
+
+  const produto = await Produto.findByPk(id)
+  if(produto == null){
+    res.status(404).json("Produto não encontrado/inexistente")
+
+  }if(descricao != null){
+    produto.descricao = descricao
+
+  }if(preco != null){
+    produto.preco = preco
+
+  }
+  await produto.save()
+  res.status(200).json("Atualizado com sucesso!")
   // TODO: 404 se não existir; atualize só os campos enviados; responda 200.
 });
 
 // DELETE /produtos/:id — remove
-router.delete("/:id", async (req, res) => {
+router.delete("/produto/:id", async (req, res) => {
+  const { id } = req.params
+
+  const produto = await Produto.findByPk(id)
+  if(produto == null){
+    res.status(404).json("Produto não encontraod/inexistente")
+  }else{
+    await produto.destroy();
+    res.status(204).json("Deletado com sucesso!")
+  }
   // TODO: 404 se não existir; senão remova e responda 204 (sem corpo).
 });
 
